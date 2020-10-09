6948908bf7e50cac3300541c48e13d16c9c6607c
kavy0701 <43661996+kavy0701@users.noreply.github.com>
2020-10-09 16:46:48 +0530

    Create userapplyleave.php

---
+++ userapplyleave.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apply Leave</title>
+
+    <?php include 'connection.php' ?>
+    <?php include 'links.php' ?>
+    <?php include 'header.php' ?>
+    <style>
+      <?php include 'css/header.css'; ?>
+      <?php include 'css/applyleave.css';?>
+    </style>
+
+</head>
+<body>
+
+
+<div class="container">            <!-- container starts-->
+<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+    <h3 class="row justify-content-center">Apply Leave</h3>
+    <br>
+    
+    <form method="POST">           <!--form-->
+    <div class="form-label-group">
+        <label for="leave">Leave Type</label> <input type="text" name="leave_type" id="leave_type" class="form-control" required>
+    </div>
+    <br>
+    
+    <div class="form-label-group">
+        <label for="duration">Duration</label>
+        <select name="duration" id="duration" class="form-control" required>
+            <option value="full_day">Full Day</option>
+            <option value="half_day">Half Day</option>
+        </select>
+    </div>
+    <br>
+    
+    <div class="form-label-group">
+        <label for="from_date">From Date</label> <input type="date" name="from_date" id="from_date" class="form-control" required>
+    </div>
+    <br>
+    
+    <div class="form-label-group">
+        <label for="to_date">To Date</label> <input type="date" name="to_date" id="to_date" class="form-control" required>
+    </div>
+    <br>
+
+    <div class="form-label-group">
+        <label for="reason">Reason</label> <textarea class="form-control" rows="3" cols="20" required></textarea>
+    </div>
+    <br>
+    
+    <button class="btn btn-primary" type="button" name="submit">Submit</button>
+
+</form>                  <!--form ends-->
+
+</div>
+</div>                   <!--container ends-->
+  
+</body>
+</html>
