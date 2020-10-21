29c253d3ceb584a8fc99f3881fa2dc59e7a94ba3
kavy0701 <43661996+kavy0701@users.noreply.github.com>
2020-10-21 16:54:51 +0530

    Update userstatus.php

--- userstatus.php
+++ userstatus.php
@@ -1,4 +1,7 @@
+ <?php
+session_start();
 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +11,7 @@
     <title>Status</title>
 
     <?php include 'links.php' ?>
-    <?php include 'header.php' ?>
+    
     <style>
       <?php include 'css/header.css'; ?>
       <?php include 'css/status.css'; ?>
@@ -17,9 +20,11 @@
 </head>
 <body>
 
-<h3>Status of your leave</h3>
+<?php include 'header.php' ?>
+
+<h3 class="h3">Status</h3>
 <br>
-<table class="table table-bordered">
+<table  class="table table-bordered">
 <thead class="thead-light">
       <tr>
         <th scope="col">no</th>
@@ -31,30 +36,59 @@
         <th scope="col">status</th>
       </tr>
     </thead>
+    
+<?php
+
+$no=1;
+?>
 
 <?php
-include 'connection.php';
+    include 'connection.php';
+
+if($_SESSION['is_login']){
+  $email = $_SESSION['email'];
+}else{
+  header("Location:index.php");
+}
 
 
 
-
-$sql = "select no,leave_type,duration,from_date,to_date,reason from apply_leave";
-$result = $con->query($sql);
-
+//code selects mentioned fields based on email
+$sql="select $no,leave_type,duration,from_date,to_date,reason,status from apply_leave where email='$email'";
+$result= $con->query($sql);
 if($result->num_rows>0){
   while($row = $result->fetch_assoc()){
-    echo "<tr><td>" . $row["no"] . "</td><td>" . $row["leave_type"] . "</td><td>" . $row["duration"] . "</td><td>" . $row["from_date"] . "</td><td>" . $row["to_date"] . "</td><td>" . $row["reason"] . "</td></tr>" ;
+    echo "<tr><td>". $no . "</td><td>"   
+    . $row["leave_type"] . "</td><td>" . $row["duration"] . 
+    "</td><td>" . $row["from_date"] . "</td><td>" . $row["to_date"] . 
+    "</td><td>" . $row["reason"] . "</td><td>"  . $row["status"] . "</td></tr>";
+    
+    $no++;
   }
-  echo "</table>";
+
+  
+  
+    echo "</table>";
+    
 }
 else{
-  echo "0 result";
+  echo "no results";
 }
+
+
+
+
+
 $con->close();
 
+
+
 ?>
+
+
 
 </table>
     
 </body>
 </html>
+
