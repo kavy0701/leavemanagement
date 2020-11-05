2ee2e97289b347ef788b9a988e3446094bcf1d67
kavy0701 <43661996+kavy0701@users.noreply.github.com>
2020-11-05 16:12:19 +0530

    Update userstatus.php

--- userstatus.php
+++ userstatus.php
@@ -1,6 +1,13 @@
- <?php
+<?php
 session_start();
 
+  include 'C:\xampp\htdocs\onlineleavemanagemet\connection.php';
+
+  if($_SESSION['is_login']){
+    $email = $_SESSION['email'];
+  }else{
+    header("Location:index.php");
+  }
 ?>
 
 <!DOCTYPE html>
@@ -10,85 +17,88 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Status</title>
 
-    <?php include 'links.php' ?>
+    <?php include 'C:\xampp\htdocs\onlineleavemanagemet\links.php' ?>
     
     <style>
-      <?php include 'css/header.css'; ?>
-      <?php include 'css/status.css'; ?>
+      <?php include 'C:\xampp\htdocs\onlineleavemanagemet\user\header.css'; ?>
+      <?php include 'C:\xampp\htdocs\onlineleavemanagemet\user\applied_status.css'; ?>
     </style>
 
 </head>
 <body>
 
-<?php include 'header.php' ?>
+  <div class="backgroundimg">
 
-<h3 class="h3">Status</h3>
-<br>
-<table  class="table table-bordered">
-<thead class="thead-light">
-      <tr>
-        <th scope="col">no</th>
-        <th scope="col">leave_type</th>
-        <th scope="col">duration</th>
-        <th scope="col">from_date</th>
-        <th scope="col">to_date</th>
-        <th scope="col">reason</th>
-        <th scope="col">status</th>
-      </tr>
-    </thead>
-    
-<?php
+    <?php include 'C:\xampp\htdocs\onlineleavemanagemet\user\header.php' ?>
 
-$no=1;
-?>
+    <h3 class="h3">Status of Applied Leaves</h3>
+    <br>
+    <div class="mx-5 mt-4 text-center">
+      <table  class="table table-bordered text-center">
+      <thead class="thead">
+        <tr>
+          <th scope="col">No</th>
+          <th scope="col">Leave Type</th>
+          <th scope="col">Duration</th>
+          <th scope="col">From</th>
+          <th scope="col">To</th>
+          <th scope="col">Reason</th>
+          <th scope="col">Status</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+      <?php
 
-<?php
-    include 'connection.php';
+        $no=1;
+      ?>
 
-if($_SESSION['is_login']){
-  $email = $_SESSION['email'];
-}else{
-  header("Location:index.php");
-}
+      <?php
+        include 'C:\xampp\htdocs\onlineleavemanagemet\connection.php';
 
-
-
-//code selects mentioned fields based on email
-$sql="select $no,leave_type,duration,from_date,to_date,reason,status from apply_leave where email='$email'";
-$result= $con->query($sql);
-if($result->num_rows>0){
-  while($row = $result->fetch_assoc()){
-    echo "<tr><td>". $no . "</td><td>"   
-    . $row["leave_type"] . "</td><td>" . $row["duration"] . 
-    "</td><td>" . $row["from_date"] . "</td><td>" . $row["to_date"] . 
-    "</td><td>" . $row["reason"] . "</td><td>"  . $row["status"] . "</td></tr>";
-    
-    $no++;
-  }
-
-  
-  
-    echo "</table>";
-    
-}
-else{
-  echo "no results";
-}
+        if($_SESSION['is_login']){
+          $email = $_SESSION['email'];
+        }else{
+          header("Location:index.php");
+        }
 
 
 
+        //code selects mentioned fields based on email
+        $selectquery = "select $no,leave_type,duration,from_date,to_date,reason,status from applied_leave where email='$email'";
+        $query = mysqli_query($con,$selectquery);
+        $num = mysqli_num_rows($query);
 
+        if($num){
+          while($res = mysqli_fetch_assoc($query)){
+      ?>
+          <tr>
+            <td><?php echo $no ?> </td>
+            <td><?php echo $res['leave_type']; ?> </td>
+            <td><?php echo $res['duration']; ?> </td>
+            <td><?php echo $res['from_date']; ?> </td>
+            <td><?php echo $res['to_date']; ?> </td>
+            <td><?php echo $res['reason']; ?> </td>
+            <td><?php echo $res['status']; ?> </td>
+            <td><a href="deleteleave.php?id=<?php echo $res['leave_type'];?>" data-toggle="tooltip" data-placement="top" title="DELETE"><i class="fas fa-trash"></i></a></td>              
+          </tr>
+      <?php $no++; ?>
 
-$con->close();
+      <?php
+       }
+      }
+                    
+      ?>
 
-
-
-?>
-
-
-
-</table>
-    
+    </tbody>            
+      </table>
+   </div>
+  </div>
+  <script>
+    $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  </script>
 </body>
 </html>
-
