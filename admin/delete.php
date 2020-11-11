212271b5675a4c42a00e9edd01e33eb9c3891a7d
kavy0701 <43661996+kavy0701@users.noreply.github.com>
2020-11-11 10:51:14 +0530

    Create delete.php

---
+++ admin/delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'C:\xampp\htdocs\onlineleavemanagemet\connection.php';
+
+$id = $_GET['id'];
+
+$deletequery = " delete from user_register where email = '$id'";
+
+$query = mysqli_query($con,$deletequery);
+
+header("Location:manage_hod.php");
+
+?>
